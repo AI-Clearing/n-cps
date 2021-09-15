3de3c4ff561119564e3a8e41c4fb0eeca936de28
Dominik Filipiak <df@aiclearing.com>
2021-09-15 07:53:45 +0000

    Confidence thresholding n networks

--- exp.voc/voc8.res50v3+.tCPS/scripts/nCPS/sup2.sh
+++ exp.voc/voc8.res50v3+.tCPS/scripts/nCPS/sup2.sh
@@ -6,18 +6,16 @@ export NGPUS=4
 export batch_size=8
 export learning_rate=0.0025
 export snapshot_iter=1
-
-export nepochs=60
 export burnup_step=0
 export cps_weight=1.5
 export threshold=0.0
+export normalising_const=0
 
-#############
 export labeled_ratio=2
-./script.sh
-export labeled_ratio=4
-./script.sh
-export labeled_ratio=8
-./script.sh
-export labeled_ratio=16
-./script.sh
+export nepochs=60
+
+for i in 2 3 4 5 6
+do
+    export num_networks=$i    
+    ./script-ncps.sh
+done
