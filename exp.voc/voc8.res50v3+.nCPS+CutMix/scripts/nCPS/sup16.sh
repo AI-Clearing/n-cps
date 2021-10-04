30129769636cbcec282c7969a917e149d39af80b
Dominik Filipiak <df@aiclearing.com>
2021-10-04 07:49:57 +0000

    Scripts adjustment

--- exp.voc/voc8.res50v3+.nCPS+CutMix/scripts/nCPS/sup16.sh
+++ exp.voc/voc8.res50v3+.nCPS+CutMix/scripts/nCPS/sup16.sh
@@ -12,18 +12,18 @@ export threshold=0.0
 export normalising_const=0
 export resnet=50
 
-export labeled_ratio=16
-export nepochs=32
-for i in 2 3
-do
-    export num_networks=$i    
-    ./script-ncps.sh
-    sleep 5
-done
+# export labeled_ratio=16
+# export nepochs=32
+# for i in 2 3
+# do
+#     export num_networks=$i    
+#     ./script-ncps.sh
+#     sleep 5
+# done
 
 export labeled_ratio=8
 export nepochs=34
-for i in 2 3
+for i in 3
 do
     export num_networks=$i    
     ./script-ncps.sh
@@ -31,7 +31,7 @@ do
 done
 
 export labeled_ratio=4
-export nepochs=340
+export nepochs=40
 for i in 2 3
 do
     export num_networks=$i    
